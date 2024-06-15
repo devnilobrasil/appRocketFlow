441286b271d4fa906e69f35fde74fc726e728863
Nilo Brasil <dev.nilobrasil@gmail.com>
2024-06-15 23:27:34 +0100

    git deploy 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/appRocketFlow/', // ajuste este caminho conforme necessário
+  base: '/appRocketFlow/dist/', // ajuste este caminho conforme necessário
   plugins: [react()],
 })
