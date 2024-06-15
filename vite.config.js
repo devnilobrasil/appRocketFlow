c5c64e42e4408b3a0b2071e14ba206f7cabeaab7
Nilo Brasil <dev.nilobrasil@gmail.com>
2024-06-15 23:18:26 +0100

    git deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/', // ajuste este caminho conforme necessário
+  base: '/vite-react-deploy/', // ajuste este caminho conforme necessário
   plugins: [react()],
 })
